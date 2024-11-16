1746da9dbc5a87028c6565ef22b901150b5478eb
Jeevitha:R <jeevithajr2005@gmail.com>
2024-11-16 14:33:54 +0530

    dom

---
+++ mongodb02/server.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const path=require('path')
+const port=3019
+const app=express()
+app.listen(port,()=>{
+    console.log("Server started")
+})
